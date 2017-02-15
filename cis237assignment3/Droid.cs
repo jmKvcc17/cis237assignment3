741e18809532b1e21e069a6a2e984c12c84a5d98
Jesse Meachum <jmeachum17@gmail.com>
2017-02-15 17:50:12 -0500

    Started work on the Droid class

--- cis237assignment3/Droid.cs
+++ cis237assignment3/Droid.cs
@@ -6,7 +6,46 @@ using System.Threading.Tasks;
 
 namespace cis237assignment3
 {
-    class Droid
+    abstract class Droid
     {
+        // Backing Fields
+        protected string material;
+        protected string model;
+        protected string color;
+        protected decimal baseCost;
+        protected decimal totalCost;
+
+        // Constructors
+        public Droid() {}
+
+        public Droid (string Material, string Model, string Color)
+        {
+            material = Material;
+            model = Model;
+            color = Color;
+        }
+
+        // Methods
+        public override string ToString()
+        {
+            return "Material: " + material + Environment.NewLine +
+                   "Model: " + model + Environment.NewLine +
+                   "Color: " + color + Environment.NewLine +
+                   "Base Cost: " + baseCost.ToString("C") + Environment.NewLine +
+                   "Total Cost: " + totalCost.ToString("C") + Environment.NewLine;
+        }
+
+        // Determines the baseCost based on material and type. *****
+        public void CalculateBaseCost()
+        {
+
+        }
+
+        // assigns baseCost to totalCost (Required by the interface)
+        public void CalculateTotalCost()
+        {
+
+        }
+
     }
 }
